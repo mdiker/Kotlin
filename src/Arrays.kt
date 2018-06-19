6823aed469afe46e3bdfc48f1fae33ac52c8f528
mdiker <m.diker21@gmail.com>
2018-06-19 22:31:40 +0300

    as

---
+++ src/Arrays.kt
@@ -0,0 +1,22 @@
+fun main(args : Array<String>)
+{
+    var myArray = Array(5){2}
+
+    myArray[3] = 5
+    myArray[2] = 4
+    myArray[1] = 3
+
+    var sayi : String? = null
+    Integer.parseInt(sayi)
+
+    var mySecondArray = Array<Int>(5){0}
+
+   for(i in 4 downTo 0)
+   {
+       println(myArray[i])
+    }
+
+
+
+
+}
